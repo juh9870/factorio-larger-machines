1e3b10415a7a20685af5ef1c582dead1fcebca39
juh9870 <juh9870@ukr.net>
2026-02-17 22:45:43 +0200

    compat: Added compatibility with Factorio HD Age

--- data-updates.lua
+++ data-updates.lua
@@ -1,9 +1,1 @@
-local api = require("api") --[[@as LargerMachines.API]]
-
 require("compat/maraxsis")
-
-for _, data in pairs(data.raw["mod-data"]) do
-	if data.data_type == "larger-machines-definition" then
-		api.apply_to_machine(data.data)
-	end
-end
